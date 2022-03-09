20b7cbff057bd302156f6bfbf593ec389f28f362
Teresa Gomez Mena <t.gomezmena@student.tudelft.nl>
2022-03-09 16:56:01 +0100

    first connections to graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { PostCard, Categories, PostWidget} from '../components'
+import {getPosts} from '../services'
 
-const posts = [
-  {title: 'React Testing', excerpt: 'Learn React Testing'}, 
-  {title: 'React Testing with Tailwind', excerpt: 'Learn React Testing with Tailwind'}, 
-]
-const Home: NextPage = () => {
+export default function Home( {posts} ) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12"> 
       <div className="lg:col-span-8 col-span-1"> 
-      {posts.map((post,index) => <PostCard post={post} key={post.title} />)}
+        {posts.map((post) => <PostCard post={post} key={post.title} />)}
       </div>
         <div className="lg:col-span-4 col-span-1"> 
           <div className="lg:sticky relative top-8">
@@ -29,4 +24,10 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts}
+  }
+}
+
