cc3907f8599abb8a43cf9a1f8ee3fc45ba34f876
Teresa Gomez Mena <t.gomezmena@student.tudelft.nl>
2022-03-09 16:06:25 +0100

    basic app

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { PostCard, Categories, PostWidget} from '../components'
+
+const posts = [
+  {title: 'React Testing', excerpt: 'Learn React Testing'}, 
+  {title: 'React Testing with Tailwind', excerpt: 'Learn React Testing with Tailwind'}, 
+]
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Team Epoch</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12"> 
+      <div className="lg:col-span-8 col-span-1"> 
+      {posts.map((post,index) => <PostCard post={post} key={post.title} />)}
+      </div>
+        <div className="lg:col-span-4 col-span-1"> 
+          <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
